9b207ce0264e15ae76ae548bc8a94f68997cc748
Monalisa Menezes <monalisa.menezes@onesight.com.br>
2022-11-06 13:35:41 -0300

    initial commit

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,67 +1,13 @@
-import express, { Express } from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
-import { ValidateError } from 'tsoa';
-import { RegisterRoutes } from '../tsoa/routes';
-import '@database';
+import "reflect-metadata";
+import express from "express";
+import "express-async-errors";
+import "dotenv/config";
 
-const app: Express = express();
-
-/************************************************************************************
- *                              Basic Express Middlewares
- ***********************************************************************************/
-
-app.set('json spaces', 4);
+const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Handle logs in console during development
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-  app.use(cors());
-}
-
-// Handle security and origin in production
-if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'production') {
-  app.use(helmet());
-}
-
-/************************************************************************************
- *                               Register all routes
- ***********************************************************************************/
-
-RegisterRoutes(app);
-
-app.use("/docs", swaggerUi.serve, async (req: express.Request, res: express.Response) => {
-  return res.send(swaggerUi.generateHTML(await import("../tsoa/swagger.json")));
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-/************************************************************************************
- *                               Express Error Handling
- ***********************************************************************************/
-
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  if (err instanceof ValidateError) {
-    console.error(`Caught Validation Error for ${req.path}:`, err.fields);
-    return res.status(422).json({
-      message: "Validation Failed",
-      details: err?.fields,
-    });
-  }
-  if (err instanceof Error) {
-    return res.status(500).json({
-      errorName: err.name,
-      message: err.message,
-      stack: err.stack || 'no stack defined'
-    });
-  }
-  next();
-});
-
-app.use(function notFoundHandler(_req, res: express.Response) {
-  return res.status(404).send({ message: "Not Found" });
-});
-
-export default app;
+export default app

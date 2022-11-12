660345bcd60b0e8ee98f9ac1f488adb716d880f6
Monalisa Menezes <monalisa.menezes@onesight.com.br>
2022-11-11 23:59:44 -0300

    fix: ajust cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,5 +1,72 @@
+import { Background, Container, Content, AnimationContainer } from "./style";
+import { FiUser, FiLock } from "react-icons/fi";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useContext } from "react";
+import { UserContext } from "../../providers/user";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return <div>Login</div>;
+  const schema = yup.object().shape({
+    username: yup.string().required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(8, "mínimo 8 digitos")
+      .required("Campo obrigatório"),
+  });
+
+  const { loginUser } = useContext(UserContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const submit = ({ username, password }) => {
+    loginUser({ username, password });
+  };
+
+  return (
+    <Container>
+      <Content>
+        <AnimationContainer>
+          <form onSubmit={handleSubmit(submit)}>
+            <h1> {'<Treinadev />'} Login</h1>
+
+            <Input
+              icon={FiUser}
+              label="Username"
+              placeholder="Seu username"
+              register={register}
+              name="username"
+              error={errors.username?.message}
+            />
+            <Input
+              icon={FiLock}
+              label="Senha"
+              type="password"
+              placeholder="Uma senha bem segura"
+              register={register}
+              error={errors.password?.message}
+              name="password"
+            />
+
+            <Button type="submit" purpleSchema>Enviar</Button>
+            <p>
+              Não tem uma conta? Faça seu <Link to="/signup">cadastro</Link>
+            </p>
+          </form>
+        </AnimationContainer>
+      </Content>
+
+      <Background />
+    </Container>
+  );
 };
 
 export default Login;
